e4dfcb64b27d4a3a9809298467d6c2c40a45de13
yuedianchaomei <1242438157@qq.com>
2026-04-20 17:06:36 +0800

    带stop的SafeQueue

--- SafeQueue.h
+++ SafeQueue.h
@@ -1,18 +1,18 @@
 #pragma once
 
-
 template<typename T>
 class SafeQueue
 {
-private:    
+private:
     std::queue<T> queue;
     mutable std::mutex mutex;
     std::condition_variable cv;
+    bool stop = false;
 
 public:
     SafeQueue() = default;
-    SafeQueue(const SafeQueue &) = delete;
-    SafeQueue & operator=(const SafeQueue &) = delete;
+    SafeQueue(const SafeQueue&) = delete;
+    SafeQueue& operator=(const SafeQueue&) = delete;
 
     bool empty() const
     {
@@ -26,26 +26,38 @@ public:
         return queue.size();
     }
 
+    void setStop(bool val)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        stop = val;
+        cv.notify_all();
+    }
+
     template<typename U>
-    void push(U &&val)
+    void push(U&& val)
     {
         std::lock_guard<std::mutex> lock(mutex);
         queue.emplace(std::forward<U>(val));
         cv.notify_one();
     }
 
-    void pop(T &val)
+    bool pop(T& val)
     {
         std::unique_lock<std::mutex> lock(mutex);
         cv.wait(lock, [this]()
         {
-            return !queue.empty();
+            return stop || !queue.empty();
         });
+        if (stop && queue.empty())
+        {
+            return false;
+        }
         val = std::move(queue.front());
         queue.pop();
+        return true;
     }
 
-    bool tryPop(T &val)
+    bool tryPop(T& val)
     {
         std::lock_guard<std::mutex> lock(mutex);
         if (queue.empty())
@@ -60,10 +72,11 @@ public:
     bool waitPop(T &val, std::chrono::milliseconds timeout)
     {
         std::unique_lock<std::mutex> lock(mutex);
-        if (!cv.wait_for(lock, timeout, [this]()
-            {
-                return !queue.empty();
-            }))
+        bool canPop = cv.wait_for(lock, timeout, [this]()
+        {
+            return stop || !queue.empty();
+        });
+        if (!canPop || (stop && queue.empty()))
         {
             return false;
         }
@@ -71,5 +84,4 @@ public:
         queue.pop();
         return true;
     }
-    
 };
